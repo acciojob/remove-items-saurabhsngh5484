c94113d1e1cf17bb8ac5591aece1360c1b34cb03
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-04-18 09:42:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.querySelector(".btn");
+
+
+function removecolor()
+{
+var x=document.getElementById("colorSelect");
+x.remove(x.selectedIndex);
+}
+
+btn.addEventListener("click",removecolor);
